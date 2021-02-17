ab04e01c8b1629d2109221c3515c050df1c3e6fc
Kozhevnikov Peter <kozhevnikov@myriad-corp.com>
2021-02-17 14:18:25 +0300

    Update ChiToken.sol

--- contracts/ChiToken.sol
+++ contracts/ChiToken.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/math/Math.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "OpenZeppelin/openzeppelin-contracts@3.0.0/contracts/math/Math.sol";
+import "OpenZeppelin/openzeppelin-contracts@3.0.0/contracts/math/SafeMath.sol";
+import "OpenZeppelin/openzeppelin-contracts@3.0.0/contracts/token/ERC20/IERC20.sol";
 
 
 abstract contract ERC20WithoutTotalSupply is IERC20 {
